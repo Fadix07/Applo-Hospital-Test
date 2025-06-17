25dd814286b42f94257115919d80e89e320997d9
shabeerali <shabeer3508@gmail.com>
2025-06-17 23:00:40 +0530

    Add .gitignore, input.css, and output.css files; updated HTML files for layout improvements and added new specialty cards in specialities.html.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	prefix: "tw-", // All Tailwind classes will start with "tw-"
+	content: ["./*.html", "./assets/**/*.js", "./assets/**/*.css"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
